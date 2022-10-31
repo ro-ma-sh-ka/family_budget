3a013ead846ecf7acb3fce98f8c9d6388c0df345
romashka <roma.l@mail.ru>
2022-10-31 22:16:26 +0400

    started with authorisation

--- budgetlesh/urls.py
+++ budgetlesh/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
 
 from budgetdb.views import startpage_view, add_expenses_view, show_expenses_view
