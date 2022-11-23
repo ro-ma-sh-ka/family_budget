99b25c7dfaf5a165b5679268baa4f848872fe2ad
romashka <roma.l@mail.ru>
2022-11-23 09:00:48 +0400

    reformating is continuing

--- budget_db/views.py
+++ budget_db/views.py
@@ -13,17 +13,22 @@ from budget_db.db_access import add_expenses
 from budget_db.models import Budget
 
 
-def start_page_view(request):
+def home_view(request):
     expenses = Budget.objects.all()
-    print(type(expenses))
-    return render(request, 'budget_db/base.html', {'expenses': expenses})
+    total = 0
+    for expence in expenses:
+        total += expence.total
+    context = {
+        'expenses': total
+    }
+    return render(request, 'budget_db/base.html', context=context)
 
 
 def add_expenses_view(request):
     return render(request, 'budget_db/add_expenses.html')
 
 
-def add_past_expenses_view(request):
+def import_past_expenses_view(request):
     with open('budgetlesh.csv', 'r', newline='') as csvfile:
         past_data = csv.reader(csvfile, delimiter=',')
         past_expenses = []
@@ -47,7 +52,7 @@ def add_past_expenses_view(request):
         except Exception:
             context = f'try again {row[0]} {traceback.print_exc()}'
             return render(request, 'budgetlesh/message.html', {'filename': context})
-    return render(request, 'budgetlesh/message.html', {'filename': 'done'})
+    return render(request, 'budget_db/message.html', {'filename': 'done'})
 
 
 def add_new_expense_view(request):
@@ -79,7 +84,7 @@ def show_expenses_view(request):
         'num_pages': paginator.num_pages,
         'page_obj': page_obj
     }
-    return render(request, 'budgetlesh/show_expenses.html', context)
+    return render(request, 'budget_db/show_expenses.html', context)
 
 
 def exportcsv_view():
--- budgetlesh/settings.py
+++ budgetlesh/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 import environ
 
@@ -137,6 +137,7 @@ USE_L10N = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
